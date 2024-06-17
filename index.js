9091ae6b6444eb507f0a7d1023eac73f6e0928c8
DinhAnh2301 <dinhanh2301@gmail.com>
2024-06-16 23:55:48 -0600

    dicee challenge

---
+++ index.js
@@ -0,0 +1,49 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+switch (randomNumber1) {
+    case 1:
+        document.getElementsByClassName("img1").setAttribute("href", "./images/dice1.png");
+        break;
+    case 2:
+        document.getElementsByClassName("img1").setAttribute("href", "./images/dice2.png");
+        break;
+    case 3:
+        document.getElementsByClassName("img1").setAttribute("href", "./images/dice3.png");
+        break;
+    case 4:
+        document.getElementsByClassName("img1").setAttribute("href", "./images/dice4.png");
+        break;
+    case 5:
+        document.getElementsByClassName("img1").setAttribute("href", "./images/dice5.png");
+        break;
+    case 6:
+        document.getElementsByClassName("img1").setAttribute("href", "./images/dice6.png");
+        break;
+    default:
+        break;    
+}
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+switch (randomNumber2) {
+    case 1:
+        document.getElementsByClassName("img2").setAttribute("href", "./images/dice1.png");
+        break;
+    case 2:
+        document.getElementsByClassName("img2").setAttribute("href", "./images/dice2.png");
+        break;
+    case 3:
+        document.getElementsByClassName("img2").setAttribute("href", "./images/dice3.png");
+        break;
+    case 4:
+        document.getElementsByClassName("img2").setAttribute("href", "./images/dice4.png");
+        break;
+    case 5:
+        document.getElementsByClassName("img2").setAttribute("href", "./images/dice5.png");
+        break;
+    case 6:
+        document.getElementsByClassName("img2").setAttribute("href", "./images/dice6.png");
+        break;
+    default:
+        break;    
+}
