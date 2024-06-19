12dc6c2af9672c00a93aaa5dee4b389cb8d9e7a9
DinhAnh2301 <dinhanh2301@gmail.com>
2024-06-19 12:51:48 -0600

    finished dice game

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 switch (randomNumber1) {
     case 1:
-        document.getElementsByClassName("img1").setAttribute("href", "./images/dice1.png");
+        document.querySelectorAll("img")[0].setAttribute("src", "./images/dice1.png");
         break;
     case 2:
-        document.getElementsByClassName("img1").setAttribute("href", "./images/dice2.png");
+        document.querySelectorAll("img")[0].setAttribute("src", "./images/dice2.png");
         break;
     case 3:
-        document.getElementsByClassName("img1").setAttribute("href", "./images/dice3.png");
+        document.querySelectorAll("img")[0].setAttribute("src", "./images/dice3.png");
         break;
     case 4:
-        document.getElementsByClassName("img1").setAttribute("href", "./images/dice4.png");
+        document.querySelectorAll("img")[0].setAttribute("src", "./images/dice4.png");
         break;
     case 5:
-        document.getElementsByClassName("img1").setAttribute("href", "./images/dice5.png");
+        document.querySelectorAll("img")[0].setAttribute("src", "./images/dice5.png");
         break;
     case 6:
-        document.getElementsByClassName("img1").setAttribute("href", "./images/dice6.png");
+        document.querySelectorAll("img")[0].setAttribute("src", "./images/dice6.png");
         break;
     default:
         break;    
@@ -27,23 +27,39 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 switch (randomNumber2) {
     case 1:
-        document.getElementsByClassName("img2").setAttribute("href", "./images/dice1.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice1.png");
         break;
     case 2:
-        document.getElementsByClassName("img2").setAttribute("href", "./images/dice2.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice2.png");
         break;
     case 3:
-        document.getElementsByClassName("img2").setAttribute("href", "./images/dice3.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice3.png");
         break;
     case 4:
-        document.getElementsByClassName("img2").setAttribute("href", "./images/dice4.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice4.png");
         break;
     case 5:
-        document.getElementsByClassName("img2").setAttribute("href", "./images/dice5.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice5.png");
         break;
     case 6:
-        document.getElementsByClassName("img2").setAttribute("href", "./images/dice6.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice6.png");
         break;
     default:
+        document.querySelectorAll("img")[1].setAttribute("src", "./images/dice6.png");
         break;    
 }
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Play 1 Wins";
+}
+
+if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Play 2 Wins🚩";
+}
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").textContent = "Draw";
+}
+
+console.log(randomNumber1);
+console.log(randomNumber2);
